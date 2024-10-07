faa76524c590c1f05e0e3c5cbd1bcba9636f74d9
Mahmud Hasan Sourov <sourov1666@gmail.com>
2024-10-07 10:34:32 +0600

    Initial commit

---
+++ src/components/ui/global-nav.tsx
@@ -0,0 +1,25 @@
+import Link from 'next/link';
+
+function GlobalNav() {
+  return (
+    <nav className="sticky top-0 grid h-16 w-full grid-cols-2 items-center justify-center bg-[hsla(0,0%,100%,.8)] px-8 py-4 shadow-bottom backdrop-blur-sm backdrop-saturate-150">
+      <div className="flex justify-start text-2xl text-gray-900">
+        <Link href="/">Next 14</Link>
+      </div>
+
+      <div className="flex justify-end gap-x-2 text-gray-500">
+        <Link href="/example">Example</Link>
+        <a
+          className="ml-2 flex items-center gap-x-1"
+          href="https://github.com/bysxx/next-ts-template-tailwind"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <span>Source Code</span>
+        </a>
+      </div>
+    </nav>
+  );
+}
+
+export default GlobalNav;
